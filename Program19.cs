052cfbccf79a83476c76db688a67fe26997eed1e
SvetonosovS <tris23@bk.ru>
2022-10-09 23:47:52 +0300

    prog19

---
+++ Program19.cs
@@ -0,0 +1,34 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Clear();
+int num;
+{
+while (true) 
+    {
+    Console.Write("Введите целое пятизначное число: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+        break;
+    Console.WriteLine($"Ошибка ввода");   
+    } 
+
+Console.Write(num +" -> ");
+
+if (num <= 9999 | num >= 100000)
+    {
+    Console.WriteLine("Ошибка ввода");
+    }
+ 
+ int a1 = num / 10000;
+ int a2 = num % 10;
+ int a3 = num / 1000 % 10;
+ int a4 = num / 10 % 10;
+
+if (a1==a2 && a3==a4)
+    Console.WriteLine("Да");
+if (num >=10000 && num <=99999 && a1!=a2 && a3!=a4)
+    Console.WriteLine("Нет");
+}
